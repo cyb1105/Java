37c282d010927a36be164d20e3d8d6ed97bf78e8
RAN-CHAE-PC\Ran-chae <dbsqud1105@gmail.com>
2020-03-04 03:31:21 +0900

    java

---
+++ Javastudy/classninstance/Calculator.java
@@ -0,0 +1,19 @@
+package classninstance;
+
+class Calculator {
+    static double PI = 3.14;
+    int left, right;
+
+    public void setOprands(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public void sum() {
+        System.out.println(this.left + this.right);
+    }
+
+    public void avg() {
+        System.out.println((this.left + this.right) / 2);
+    }
+}
